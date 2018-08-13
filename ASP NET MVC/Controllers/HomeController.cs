b22f707c4f47269e0d53ff3f6341427c5c7e1eb9
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-13 15:01:11 +0400

    init commit

---
+++ ASP NET MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ASP_NET_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_NET_MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ViewResult Index()
+		{
+			var isEven = DateTime.Now.Second % 2 == 0;
+			ViewBag.IsEven = isEven;
+			return View();
+		}
+
+		[HttpGet]
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult RsvpForm(GuestResponse guest)
+		{
+			if (ModelState.IsValid)
+			{
+				return View("Thanks", guest);
+			}
+			else
+			{
+				return View();
+			}
+		}
+	}
+}
