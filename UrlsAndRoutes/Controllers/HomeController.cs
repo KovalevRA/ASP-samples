f8eb8839d16166c7b0a3bac880347de5b4c95df8
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-15 23:11:11 +0400

    UrlsAndRoutes

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+	public class HomeController : Controller
+    {
+		public ActionResult List()
+		{
+			ViewBag.Controller = "Home";
+			ViewBag.Action = "List";
+			return View("ActionName");
+		}
+
+		public ActionResult Index()
+		{
+			ViewBag.Controller = "Home";
+			ViewBag.Action = "Index";
+			return View("ActionName");
+		}
+	}
+}
--- UrlsAndRoutes/Global.asax.cs
+++ UrlsAndRoutes/Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace ASP_NET_MVC
+namespace UrlsAndRoutes
 {
-	public class MvcApplication : HttpApplication
+    public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
