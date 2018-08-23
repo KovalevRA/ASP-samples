6d98f03938dc1c5fb1fa7eb6a7f4cbae2670a823
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-23 22:18:01 +0400

    ControllersAndActions

--- ControllersAndActions/ControllersAndActions.Tests/UnitTest1.cs
+++ ControllersAndActions/ControllersAndActions.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using ControllersAndActions.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
 
 namespace ControllersAndActions.Tests
 {
@@ -9,6 +10,27 @@ namespace ControllersAndActions.Tests
 		[TestMethod]
 		public void TestMethod1()
 		{
+		}
+
+		[TestMethod]
+		public void ControllerTest()
+		{
+			ExampleController controller = new ExampleController();
+
+			ViewResult result = controller.Index();
+
+			Assert.AreEqual("Homepage", result.ViewName);
+		}
+
+		[TestMethod]
+		public void ViewSelectionTest()
+		{
+			ExampleController controller = new ExampleController();
+
+			ViewResult result = controller.Index();
+
+			Assert.AreEqual("", result.ViewName);
+			Assert.IsInstanceOfType(result.ViewData.Model, typeof(System.DateTime));
 		}
 	}
 }
---
+++ ControllersAndActions/ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,24 @@
+﻿using ControllersAndActions.Infrastructure;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+	public class DerivedController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult ProduceOutput()
+		{
+			if (Server.MachineName == "MyMachineName")
+				return new CustomRedirectResult { Url = "Basic/Index" };
+			else
+			{
+				Response.Write("Контроллер: Derived, Метод действия: ProduceOutput");
+				return null;
+			}
+		}
+    }
+}
