b727ed4eb3986e1fd04fcbf6a529387c9b7a98df
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-27 22:59:27 +0400

    Views

---
+++ ControllersAndActions/WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			string[] names = { "Яблоко", "Апельсин", "Груша" };
+			return View(names);
+		}
+
+		public ViewResult List()
+		{
+			return View();
+		}
+	}
+}
