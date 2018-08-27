1a2fead2d4f6e60228985d747c03de87eb752d70
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-27 15:24:01 +0400

    ControllerActivator

--- ControllersAndActions/ControllerExtensibility/Global.asax.cs
+++ ControllersAndActions/ControllerExtensibility/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using ControllerExtensibility.Infrastructure;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ControllerExtensibility
@@ -9,6 +10,8 @@ namespace ControllerExtensibility
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			ControllerBuilder.Current.SetControllerFactory(new DefaultControllerFactory(new CustomerControllerActivator()));
         }
     }
 }
---
+++ ControllersAndActions/ControllerExtensibility/Infrastructure/CustomerControllerActivator.cs
@@ -0,0 +1,19 @@
+﻿using ControllerExtensibility.Controllers;
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControllerExtensibility.Infrastructure
+{
+	public class CustomerControllerActivator : IControllerActivator
+	{
+		public IController Create(RequestContext requestContext, Type controllerType)
+		{
+			if (controllerType == typeof(ProductController))
+			{
+				controllerType = typeof(CustomerController);
+			}
+			return (IController)DependencyResolver.Current.GetService(controllerType);
+		}
+	}
+}
