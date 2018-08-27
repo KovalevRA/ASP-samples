f3e190f100ff146a708990050842d6cad215128b
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-27 17:14:19 +0400

    AsyncControllers

---
+++ ControllersAndActions/AsyncControllers/Controllers/RemoteDataController.cs
@@ -0,0 +1,18 @@
+﻿using AsyncControllers.Models;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AsyncControllers.Controllers
+{
+	public class RemoteDataController : Controller
+    {
+        public async Task<ActionResult> Data()
+        {
+			var data = await Task<string>.Factory.StartNew(() =>
+			{
+				return new RemoteService().GetRemoteData();
+			});
+            return View((object)data);
+        }
+    }
+}
