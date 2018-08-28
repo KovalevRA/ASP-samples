8bd9227f0204ccca489acbcfd27b3ce69f2aa5b0
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-28 23:28:07 +0400

    GameStore init

---
+++ GameStore/GameStore.WebUI/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using GameStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+	public class GameController : Controller
+    {
+		private IGameRepository repository;
+		public GameController(IGameRepository repo)
+		{
+			repository = repo;
+		}
+
+		public ViewResult List()
+		{
+			return View(repository.Games);
+		}
+    }
+}
