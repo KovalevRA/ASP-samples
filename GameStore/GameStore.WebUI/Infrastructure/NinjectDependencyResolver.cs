fb47b6b593e6e7e42e274bdbf15d6817ba7e8c44
Kovalev Roman <KovalevRA@rubezh.ru>
2018-08-30 22:32:13 +0400

    cart

--- GameStore/GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore/GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,6 +5,7 @@ using Moq;
 using Ninject;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace GameStore.WebUI.Infrastructure
@@ -46,6 +47,17 @@ namespace GameStore.WebUI.Infrastructure
 				new Game { Name = "TITANFALL 3", Price=2299, Category = "Action" },
 				new Game { Name = "Battlefield 6", Price=899.4M, Category = "Action" },
 			});
+
+			int i = 1;
+			foreach (var game in mock.Object.Games)
+			{
+				game.GameId = i++;
+				game.Description = @"Мы добавили блок Razor, который создает небольшую HTML-форму для каждого товара в списке. Отправка этой формы приводит к вызову метода действия AddToCart() из контроллера Cart (который вскоре будет реализован).
+
+По умолчанию вспомогательный метод BeginForm() создает форму, которая использует HTTP - метод POST.Это можно изменить, обеспечив работу формы с HTTP - методом GET, но следует соблюдать осторожность.В спецификации HTTP указано, что запросы GET не должны быть изменяющими, а добавление товара в корзину определенно считается изменением.
+
+Использование вспомогательного метода Html.BeginForm() в списке товаров означает, что каждая кнопка ""Добавить в корзину"" визуализируется в собственном отдельном HTML - элементе<form>.Это может поначалу удивить, если вам ранее приходилось разрабатывать приложения с помощью ASP.NET Web Forms.В ASP.NET Web Forms существует ограничение, допускающее определение только одной формы на странице, если требуется наличие средства состояния представления или сложных элементов управления(которые обычно полагаются на состояние представления).";
+			}
 			kernel.Bind<IGameRepository>().ToConstant(mock.Object);
 			//kernel.Bind<IGameRepository>().To<EFGameRepository>();
 		}
